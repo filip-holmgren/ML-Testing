0a01a859f60dddcf058e88f0f4b38f33203bb853
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2026-02-25 15:24:32 +0100

    added optuna optimization

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import xgboost as xgb
 import pandas as pd
 import numpy as np
 import warnings
+import optuna
 
 def main():
     warnings.filterwarnings("ignore")
@@ -26,38 +27,64 @@ def main():
     dtrain_reg = xgb.DMatrix(X_train, y_train, enable_categorical=True)
     dtest_reg = xgb.DMatrix(X_test, y_test, enable_categorical=True)
 
-    params = {
+    def objective(trial): 
+        params = {
+            "objective": "reg:squarederror", 
+            "tree_method": "hist",
+            "device": "cuda",
+            "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.3),
+            "max_depth": trial.suggest_int("max_depth", 3, 12),
+            "subsample": trial.suggest_float("subsample", 0.5, 1.0),
+            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.5, 1.0),
+        } # 522 latest
+
+
+        results = xgb.cv(
+            params, dtrain_reg,
+            num_boost_round=10000,
+            nfold=5,
+            early_stopping_rounds=20,
+            verbose_eval = False
+        )
+
+        return results["test-rmse-mean"].min()
+
+    study = optuna.create_study(direction="minimize")
+    study.optimize(objective, n_trials=30)
+
+    print("Best parameters: ", study.best_params)
+    print("Best RMSE (CV): ", study.best_value)
+
+    best_params = {
         "objective": "reg:squarederror", 
-        "tree_method": "auto", 
-        "learning_rate": 0.01, 
-        "max_depth": 12,
-        "subsample": 0.6,
-        "colsample_bytree": 0.8
+        "tree_method": "hist",
+        "device": "cuda",
+        **study.best_params
     }
+
+    cv_results = xgb.cv(
+        best_params,
+        dtrain_reg,
+        num_boost_round=1000,
+        nfold=5,
+        early_stopping_rounds=20,
+        verbose_eval=False
+    )
 
     evals = [(dtrain_reg, "train"), (dtest_reg, "validation")]
 
-    n = 10000
-
-    results = xgb.cv(
-        params, dtrain_reg,
-        num_boost_round=n,
-        nfold=5,
-        early_stopping_rounds=20
-    )
-
-    best_rmse = results['test-rmse-mean'].min()
-    print(results.head())
+    best_rmse = cv_results['test-rmse-mean'].min()
+    print(cv_results.head())
     print(best_rmse)
 
-    best_n_rounds = results['test-rmse-mean'].argmin() + 1
+    best_n_rounds = cv_results['test-rmse-mean'].argmin() + 1
     verbose_eval = best_n_rounds // 10
 
     print(f"Best num boost rounds {best_n_rounds}")
     print(f"Verbose eval every {verbose_eval} rounds")
 
     model = xgb.train(
-        params,
+        best_params,
         dtrain_reg,
         num_boost_round = best_n_rounds,
         evals=evals,
@@ -67,7 +94,7 @@ def main():
     preds = model.predict(dtest_reg)
 
     rmse = root_mean_squared_error(y_test, preds)
-    print(f"RMSE of the base model: {rmse:.3f}")
+    print(f"RMSE of the final model: {rmse:.3f}")
 
 
 if __name__ == "__main__":
